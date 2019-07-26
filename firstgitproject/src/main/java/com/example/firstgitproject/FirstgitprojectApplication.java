b7a36aed665650e1d072b6b31b62b6fc047eb3f5
tt gunners <you@example.com>
2019-07-26 18:42:15 -0400

    this is spring set up

---
+++ firstgitproject/src/main/java/com/example/firstgitproject/FirstgitprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstgitproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstgitprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstgitprojectApplication.class, args);
+	}
+
+}
---
+++ firstgitproject/src/test/java/com/example/firstgitproject/FirstgitprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.firstgitproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstgitprojectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
